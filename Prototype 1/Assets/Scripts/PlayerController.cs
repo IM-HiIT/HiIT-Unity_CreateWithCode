dd8040e6ca85e0f196be977f26a1430f0a07a4eb
User <I.Munster92@gmail.com>
2023-12-07 15:42:44 +0100

    1.4.2 - Base left/right movement on input
    https://learn.unity.com/tutorial/lesson-1-4-use-user-input-to-control-the-vehicle?labelRequired=true&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f71fe63edbc2a00200e9de0&projectId=5caccdfbedbc2a3cef0efe63#5cbe3969edbc2a191e639151

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,15 +6,22 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField] private float speed = 5.0f;
     [SerializeField] private float turnSpeed = 5.0f;
+
+    private float horizInput;
+    private float vertiInput;
+
     // Start is called before the first frame update
     void Start() {
     }
 
     // Update is called once per frame
     void Update() {
+        // Capture Input
+        horizInput = Input.GetAxis("Horizontal");
+        vertiInput = Input.GetAxis("Vertical");
         // Move the Vehicle Forward (20Meter per Second)
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertiInput);
         // Turn the Vehicle
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizInput);
     }
 }
