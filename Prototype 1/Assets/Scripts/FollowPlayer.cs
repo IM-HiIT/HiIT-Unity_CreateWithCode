a0edf9e456e636f47292f29722938e7ddc62f1e7
I Munster <ian.Munster@StichtingHeldenInIT.nl>
2024-01-03 11:57:30 +0100

    Editor Update

--- Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -9,7 +9,6 @@ public class FollowPlayer : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        
     }
 
     // LateUpdate is called after Update once per frame
