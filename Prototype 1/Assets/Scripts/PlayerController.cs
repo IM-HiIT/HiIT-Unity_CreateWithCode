250be9980ad6dd99b99d06204c42f4b18d9ea487
I Munster <ian.Munster@StichtingHeldenInIT.nl>
2024-01-04 14:11:46 +0100

    1.4.4 - Make vehicle rotate instead of slide
    https://learn.unity.com/tutorial/lesson-1-4-use-user-input-to-control-the-vehicle?uv=2022.3&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f71fe63edbc2a00200e9de0&projectId=5caccdfbedbc2a3cef0efe63#5cbe3969edbc2a191e639153

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    [SerializeField] private float speed = 5.0f;
-    [SerializeField] private float turnSpeed = 5.0f;
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float turnSpeed = 25.0f;
 
     private float vertiInput;
     private float horizInput;
@@ -24,6 +24,6 @@ public class PlayerController : MonoBehaviour {
         // Move the Vehicle Forward (20Meter per Second)
         transform.Translate(Vector3.forward * Time.deltaTime * speed * vertiInput);
         // Turn the Vehicle
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizInput);
     }
 }
