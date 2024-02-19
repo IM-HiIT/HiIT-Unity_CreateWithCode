eb2bd3f30f650f0126444420cdc85c7b6c7bd46f
I Munster <Ian.Munster@StichtingHeldenInIT.nl>
2024-02-19 13:32:48 +0100

    6.Clean up your code and variables
    https://learn.unity.com/tutorial/lesson-2-1-control-player-bounds-with-if-then-statements?uv=2022.3&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f71fe63edbc2a00200e9de0&projectId=5cdcc312edbc2a24a41671e6#5cbf5785edbc2a12423d5acd

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,18 +5,22 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     private float horizInput;
     [SerializeField] private float speed = 10f;
+    [SerializeField] private float boundary = 15f;
 
     // Start is called before the first frame update
     void Start() {
-        
+
     }
 
     // Update is called once per frame
     void Update() {
-        if (transform.position.x < -10) {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        if (transform.position.x < -boundary) {
+            transform.position = new Vector3(-boundary, transform.position.y, transform.position.z);
         }
-     horizInput = Input.GetAxis("Horizontal");
-     transform.Translate(horizInput * speed * Time.deltaTime * Vector3.right);
+        if (transform.position.x > boundary) {
+            transform.position = new Vector3(boundary, transform.position.y, transform.position.z);
+        }
+        horizInput = Input.GetAxis("Horizontal");
+        transform.Translate(horizInput * speed * Time.deltaTime * Vector3.right);
     }
 }
