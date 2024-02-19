90172ab249c6802e5cd2d216dcefe26bb09ef510
I Munster <Ian.Munster@StichtingHeldenInIT.nl>
2024-02-19 16:41:28 +0100

    2.Spawn an animal if S is pressed
    https://learn.unity.com/tutorial/lesson-2-3-random-animal-stampede?uv=2022.3&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f71fe63edbc2a00200e9de0&projectId=5cdcc312edbc2a24a41671e6#5cdcbf07edbc2a22e9139b51

--- Prototype 2/Assets/Scripts/spawnManager.cs
+++ Prototype 2/Assets/Scripts/spawnManager.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class spawnManager : MonoBehaviour {
 
+
     [SerializeField] private GameObject [] animalPrefabs;
+    private int animalIndex = 0;
+    [SerializeField] private Vector3 spawnPos = new Vector3(0, 0, 20);
 
     // Start is called before the first frame update
     void Start() {
@@ -13,6 +16,8 @@ public class spawnManager : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+        if (Input.GetKeyDown(KeyCode.S)) {
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
