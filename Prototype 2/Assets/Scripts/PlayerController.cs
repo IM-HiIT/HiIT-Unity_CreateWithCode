478c6e7452ff9f8cf30a99594edb4c28c4aaf851
I Munster <Ian.Munster@StichtingHeldenInIT.nl>
2024-02-19 13:28:38 +0100

    5.Keep the player inbounds
    https://learn.unity.com/tutorial/lesson-2-1-control-player-bounds-with-if-then-statements?uv=2022.3&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f71fe63edbc2a00200e9de0&projectId=5cdcc312edbc2a24a41671e6#5cbf5785edbc2a12423d5acc

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     private float horizInput;
     [SerializeField] private float speed = 10f;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        if (transform.position.x < -10) {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
      horizInput = Input.GetAxis("Horizontal");
      transform.Translate(horizInput * speed * Time.deltaTime * Vector3.right);
     }
