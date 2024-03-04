99e4c6bd7e69de99b30c1d739c6dc40feef303bc
I Munster <Ian.Munster@StichtingHeldenInIT.nl>
2024-03-04 10:42:58 +0100

    4.1

--- Prototype 2/Assets/Scripts/MoveForward.cs
+++ Prototype 2/Assets/Scripts/MoveForward.cs
@@ -1,18 +1,9 @@
-// using System.Collections;
-// using System.Collections.Generic;
 using UnityEngine;
-
-public class MoveForward : MonoBehaviour {
-
-    [SerializeField] private float speed = 40f;
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
-    // Update is called once per frame
-    void Update() {
-        transform.Translate(speed * Time.deltaTime * Vector3.forward);
-        
+public class MoveForward : MonoBehaviour
+{
+    [SerializeField] private float speed = 40f; // Movement Speed
+    void Update()
+    {                             // Update is called once per frame
+        transform.Translate(speed * Time.deltaTime * Vector3.forward);  // Move the GameObject forwards with speed 
     }
 }
